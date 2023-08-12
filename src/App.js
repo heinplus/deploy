491125e13e55209747eab638bf7d6f11bb9c7c91
heinAstonish <heinplushtet@gmail.com>
2023-08-12 21:31:24 +0630

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <h1>I Am Gloria Xenon</h1>
         </p>
         <a
           className="App-link"
